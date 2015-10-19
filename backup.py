cdfba4f34fe05a95bcd796a39ba93c1a8b542b8b
Alex Seiler <seileralex@gmail.com>
2015-10-19 18:26:26 +0200

    ...

--- backup.py
+++ backup.py
@@ -27,8 +27,27 @@ class Backup:
         self.fullBackupFilenameExtension = 'full'
         self.incrementalBackupFilenameExtension = 'incremental'
 
+
     def fullBackupAvailable(self):
-        pass
+        """
+        This method checks, if there is a full backup of all the specified
+        backup entries available. It returns 'True', if there is a full backup
+        available, and 'False' otherwise.
+        """
+        files = os.listdir(self.backupDirectory)
+        for entry in self.backupEntries:
+            found = False
+            name = entry.getName()
+            fileExtension = entry.getFilenameExtension()
+            regexString = name + '_\d\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d_' + self.fullBackupFilenameExtension + fileExtension
+            for f in files:
+                if re.match(regexString, f):
+                    found = True
+                    break
+            if not found:
+                return False
+        return True
+
 
 
     def performBackup(self):
