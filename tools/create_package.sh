baebbfe8e93da3870a1d374f011c45a51cd8ef52
Alex Seiler <seileralex@gmail.com>
2015-10-26 12:06:57 +0100

    Got release build script to work again...

--- tools/create_package.sh
+++ tools/create_package.sh
@@ -5,12 +5,12 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && cd .. && cd .. && pwd  )"
 PROGRAMDIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && cd .. && pwd  )"
-DIRNAME="backup"
+DIRNAME="backupper"
 
 
 find $PROGRAMDIR | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 
 if [[ -n "$1" ]]; then
-	tar cfvJ ${BASEDIR}/backupper-${1}.tar.xz --exclude .git --exclude .gitignore -C ${BASEDIR} --transform 's,'${DIRNAME}',backupper-'$1',' backup
+	tar cfvJ ${BASEDIR}/backupper-${1}.tar.xz --exclude .git --exclude .gitignore -C ${BASEDIR} --transform 's,'${DIRNAME}',backupper-'$1',' ${DIRNAME}
 fi
 
