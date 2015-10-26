6459ca967c13181eb167453df43cd7af2c02617c
Alex Seiler <seileralex@gmail.com>
2015-10-26 12:04:44 +0100

    Added installation of the configuration file.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='backupper',
-    version='0.1.1',
+    version='0.1.2',
     description='A utility to automatically create full and incremental backups.',
     author='Alex Seiler',
     author_email='seileralex@gmail.com',
     url='https://github.com/goggle/backupper',
     packages=['Backupper'],
-    scripts=['backupper'])
+    scripts=['backupper'],
+    data_files=[('/etc', ['backupper.conf'])]
+    )
